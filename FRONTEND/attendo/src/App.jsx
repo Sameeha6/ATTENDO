4334be79d24292b114e3c792862cbf0a47212aef
Sameeha T <sameehasirajt@gmail.com>
2025-02-22 15:32:47 +0530

    try

--- FRONTEND/attendo/src/App.jsx
+++ FRONTEND/attendo/src/App.jsx
@@ -1,15 +1,4 @@
-<<<<<<< HEAD
-import { useState } from "react";
-import "./App.css";
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/home";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Login from "./pages/Login";
-import AboutUs from "./pages/About";
-import ContactUs from "./pages/Contact";
-import AdminRoutes from "./routes/AdminRoutes";
-=======
+
 import { useState } from 'react'
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
@@ -21,7 +10,7 @@ import AboutUs from './pages/About'
 import ContactUs from './pages/Contact'
 import AdminRoutes from './routes/AdminRoutes'
 import HodRoutes from './routes/HODroutes'
->>>>>>> 4a68f10be941b28dd7cc34a45943de2864d7c61a
+
 
 function App() {
   const [count, setCount] = useState(0);
@@ -39,14 +28,10 @@ function App() {
     //   </div>
     //   <Footer />
     // </div>
-<<<<<<< HEAD
-    <AdminRoutes />
-  );
-=======
     // <AdminRoutes/>
     <HodRoutes/>
   )
->>>>>>> 4a68f10be941b28dd7cc34a45943de2864d7c61a
+
 }
 
 export default App;
